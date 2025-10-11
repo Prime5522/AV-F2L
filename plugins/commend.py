e4ab98ff50fc47c1d988de1aeae10eb549232cd2
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-10-11 06:52:41 +0600

    Update commend.py

--- plugins/commend.py
+++ plugins/commend.py
@@ -384,19 +384,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
 	)
 
-	elif query.data == "source_prime":
-        await query.message.delete()
-        await query.message.reply_photo(
-            photo="https://i.postimg.cc/hvFZ93Ct/file-000000004188623081269b2440872960.png",
-            caption="⚠️ Private Source Project info...",
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("♚ ᴀᴅᴍɪɴ ♚", url="https://t.me/Prime_Admin_Support_ProBot")],
-                [InlineKeyboardButton("• ᴄʟᴏsᴇ •", callback_data="closes")]
-            ])
-        )
-
-    elif query.data == "closes":
-        await query.message.delete()
 
     # ⏩ Pagination: Next/Back
     elif query.data.startswith("filespage_"):
@@ -576,3 +563,40 @@ async def help(client, message):
         reply_markup=reply_markup
 		)
 
+
+@Client.on_callback_query()
+async def cb_handler_2(client, query):
+    user_id = query.from_user.id
+    if query.data == "closes":
+        try:
+            await query.message.delete()
+        except Exception:
+            await query.answer("⚠️ Cannot delete message.", show_alert=True)
+        return  # exit early
+
+    elif query.data == "source_prime":   # ← নতুন callback_data
+        try:
+            # প্রথমে আগের মেসেজ ডিলিট হবে
+            await query.message.delete()
+        except Exception:
+            pass
+
+        # এখন নতুন করে ছবি + ক্যাপশন পাঠানো হবে
+        await query.message.reply_photo(
+            photo="https://i.postimg.cc/hvFZ93Ct/file-000000004188623081269b2440872960.png",
+            caption=(
+                f"👋 Hello Dear 👋,\n\n"
+                "⚠️ ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ᴀ ᴘʀɪᴠᴀᴛᴇ ꜱᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ\n\n"
+                "ᴛʜɪs ʙᴏᴛ ʜᴀs ʟᴀsᴛᴇsᴛ ᴀɴᴅ ᴀᴅᴠᴀɴᴄᴇᴅ ꜰᴇᴀᴛᴜʀᴇs⚡️\n"
+                "▸ ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ꜱᴏᴜʀᴄᴇ ᴄoᴅᴇ oʀ ʟɪᴋᴇ ᴛʜɪꜱ ʙᴏᴛ ᴄᴏɴᴛᴀᴄᴛ ᴍᴇ..!\n"
+                "▸ ɪ ᴡɪʟʟ ᴄʀᴇᴀᴛᴇ ᴀ ʙᴏᴛ ꜰᴏʀ ʏᴏᴜ oʀ ꜱᴏᴜʀᴄᴇ ᴄoᴅᴇ\n"
+                "⇒ ᴄᴏɴᴛᴀᴄᴛ ᴍᴇ - ♚ ᴀᴅᴍɪɴ ♚."
+            ),
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton("♚ ᴀᴅᴍɪɴ ♚", url="https://t.me/Prime_Admin_Support_ProBot")],
+                    [InlineKeyboardButton("• ᴄʟᴏsᴇ •", callback_data="closes")]
+                ]
+            )
+		)
+		
